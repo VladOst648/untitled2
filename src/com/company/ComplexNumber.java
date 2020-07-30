0a6b507ec5105afc51d80ad41feba400eae708d4
VladOst648 <ostaschovvlad@yandex.ru>
2020-07-30 20:02:48 +0300

    Version 3.

--- src/com/company/ComplexNumber.java
+++ src/com/company/ComplexNumber.java
@@ -2,51 +2,34 @@ package com.company;
 import java.util.Scanner;
 
 public class ComplexNumber {
-    private double realPart;
-    private double imaginaryPart;
+     double realPart;
+     double imaginaryPart;
 
     public ComplexNumber(double rp, double ip) {
+
         realPart = rp;
         imaginaryPart = ip;
     }
 
-    public void addition(ComplexNumber n) {
+    public ComplexNumber addition(ComplexNumber n) {
+
         double a = n.realPart + this.realPart;
         double b = n.imaginaryPart + this.imaginaryPart;
-        print(a, b);
+        ComplexNumber sum = new ComplexNumber(a,b);
+        return sum;
     }
 
-    public void Substraction(ComplexNumber n) {
+    public ComplexNumber substraction(ComplexNumber n) {
         double a = this.realPart - n.realPart;
         double b = this.imaginaryPart - n.imaginaryPart;
-        print(a, b);
+        ComplexNumber difference = new ComplexNumber(a, b);
+        return difference;
     }
 
-    public void multiplicate(ComplexNumber n) {
+    public ComplexNumber multiplicate(ComplexNumber n) {
         double a = n.realPart * this.realPart - n.imaginaryPart * this.imaginaryPart;
         double b = n.realPart * this.imaginaryPart + n.imaginaryPart * this.realPart;
-        print(a, b);
-    }
-
-    private void print(double a, double b) {
-        if ((a != 0) && (b != 0)) {
-            if (b<0) {
-                System.out.println(a + " - " + (-b) + 'i');
-            }
-            else if (b>0) {
-                System.out.println(a + " + " + b + 'i');
-            }
-        }
-        else if ((a == 0) && (b == 0)) {
-            System.out.println(0);
-        }
-
-        else if ((a == 0) && (b != 0)) {
-            System.out.println(b + 'i');
-        }
-
-        else if ((a != 0) && (b == 0)) {
-            System.out.println(a);
-        }
+        ComplexNumber product = new ComplexNumber(a,b);
+        return product;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,11 +5,36 @@ import java.util.zip.CheckedOutputStream;
 
 public class Main {
 
-    public static void main(String[] args) {
+	public static void print (ComplexNumber cn) {
+
+		if ((cn.realPart != 0) && (cn.imaginaryPart != 0)) {
+			if (cn.imaginaryPart<0) {
+				System.out.println(cn.realPart + " - " + (-cn.imaginaryPart) + 'i');
+			} else if (cn.imaginaryPart>0) {
+				System.out.println(cn.realPart + " + " + cn.imaginaryPart + 'i');
+			}
+		} else if ((cn.realPart == 0) && (cn.imaginaryPart == 0)) {
+			System.out.println(0);
+		} else if ((cn.realPart == 0) && (cn.imaginaryPart != 0)) {
+			System.out.println(cn.imaginaryPart + 'i');
+		} else if ((cn.realPart != 0) && (cn.imaginaryPart == 0)) {
+			System.out.println(cn.realPart);
+		}
+	}
+
+	public static void main(String[] args) {
+
 		ComplexNumber one = new ComplexNumber(-1.5,2.5);
+		System.out.print("Первое число: ");
+		print(one);
 		ComplexNumber two = new ComplexNumber(3.5,-4.5);
-		one.addition(two);
-		one.Substraction(two);
-		one.multiplicate(two);
-    }
+		System.out.print("Второе число: ");
+		print(two);
+		System.out.print("Их сумма: ");
+		print(one.addition(two));
+		System.out.print("Их разность: ");
+		print(one.substraction(two));
+		System.out.print("Их произведение: ");
+		print(one.multiplicate(two));
+	}
 }
