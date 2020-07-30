14cc75190c954c8bbe2bbafd3142ff8f7fb6e96d
VladOst648 <ostaschovvlad@yandex.ru>
2020-07-30 16:02:37 +0300

    Changed and fixed.

--- src/com/company/ComplexNumber.java
+++ src/com/company/ComplexNumber.java
@@ -2,249 +2,51 @@ package com.company;
 import java.util.Scanner;
 
 public class ComplexNumber {
-    String RealPart;
-    String ImaginaryPart;
-    private void Print(int A, int B) {
-        if ((A != 0) && (B != 0)) {
-            if (B < 0) {
-                System.out.println(A + "" + B + "i");
+    private double realPart;
+    private double imaginaryPart;
+
+    public ComplexNumber(double rp, double ip) {
+        realPart = rp;
+        imaginaryPart = ip;
+    }
+
+    public void addition(ComplexNumber n) {
+        double a = n.realPart + this.realPart;
+        double b = n.imaginaryPart + this.imaginaryPart;
+        print(a, b);
+    }
+
+    public void Substraction(ComplexNumber n) {
+        double a = this.realPart - n.realPart;
+        double b = this.imaginaryPart - n.imaginaryPart;
+        print(a, b);
+    }
+
+    public void multiplicate(ComplexNumber n) {
+        double a = n.realPart * this.realPart - n.imaginaryPart * this.imaginaryPart;
+        double b = n.realPart * this.imaginaryPart + n.imaginaryPart * this.realPart;
+        print(a, b);
+    }
+
+    private void print(double a, double b) {
+        if ((a != 0) && (b != 0)) {
+            if (b<0) {
+                System.out.println(a + " - " + (-b) + 'i');
             }
-            else if (B>0){
-                int R = A; int D = B;
-                System.out.println(A + "+" + B + "i");
+            else if (b>0) {
+                System.out.println(a + " + " + b + 'i');
             }
         }
-        else if ((A == 0) && (B != 0)) {
-            System.out.println(B+"i");
-        }
-        else if ((B == 0) && (A != 0)) {
-            System.out.println(A);
-        }
-        else if ((B == 0) && (A == 0)) {
+        else if ((a == 0) && (b == 0)) {
             System.out.println(0);
         }
-    }
-    private void Print(double A, double B) {
-        if ((A != 0) && (B != 0)) {
-            if (B < 0) {
-                System.out.println(A + "" + B + "i");
-            }
-            else if (B>0){
-                System.out.println(A + "+" + B + "i");
-            }
-        }
-        else if ((A == 0) && (B != 0)) {
-            System.out.println(B+"i");
-        }
-        else if ((B == 0) && (A != 0)) {
-            System.out.println(A);
-        }
-        else if ((B == 0) && (A == 0)) {
-            System.out.println(0);
-        }
-    }
-    public void Entering() {
-        System.out.print("Действительная чать: ");
-        Scanner sc = new Scanner(System.in);
-        RealPart = sc.nextLine();
-        System.out.print("Мнимая часть: ");
-        Scanner sc1 = new Scanner(System.in);
-        ImaginaryPart = sc1.nextLine();
-        System.out.println("Полученное комплексное число: ");
-        char[] IP = ImaginaryPart.toCharArray();
-        if (IP[0] == '-') {
-            System.out.println(RealPart+""+ImaginaryPart);
-        }
-        else {
-            System.out.println(RealPart+"+"+ImaginaryPart);
-        }
-    }
-    public boolean IsDouble(ComplexNumber N) {
 
-        char[] RP = RealPart.toCharArray();
-
-        for (int i = 0; i < RP.length; i++) {
-            if ((RP[i] == ',') || (RP[i] == '.')) {
-                return true;
-            }
+        else if ((a == 0) && (b != 0)) {
+            System.out.println(b + 'i');
         }
-        char[] IP = ImaginaryPart.toCharArray();
-        for (int i = 0; i < IP.length; i++) {
-            if ((IP[i] == '.') || (IP[i] == ',')) {
-                return true;
-            }
-        }
-        return false;
-    }
-    char[] NutNumb = {'1','2','3','4','5','6','7','8','9',',','.','-'};
-    public boolean inArray(char c, char[] NN) {
-        for (int i = 0; i < NN.length; i++) {
-            if (c == NN[i]) {
-                return true;
-            }
-        }
-        return false;
-    }
-    public void Addition(ComplexNumber N) {
-        if (IsDouble(N) == true) {
-            //---------------------------------
-            double A1 = Double.parseDouble(this.RealPart);
-            double A2 = Double.parseDouble(N.RealPart);
 
-            String l1 = "";
-            char[] ds = this.ImaginaryPart.toCharArray();
-            for (int i = 0; i < this.ImaginaryPart.length(); i++) {
-                if (inArray(ds[i],NutNumb) == true) {
-                    l1 += ds[i];
-                }
-            }
-            int B1 = Integer.parseInt(l1);
-
-            String l2 = "";
-            char[] dc = N.ImaginaryPart.toCharArray();
-            for (int j = 0; j < N.ImaginaryPart.length(); j++) {
-                if (inArray(dc[j],NutNumb) == true) {
-                    l2 += dc[j];
-                }
-            }
-            double B2 = Double.parseDouble(l2);
-            //---------------------------------
-            double B = B1+B2; double A = A1+A2;
-            System.out.print("Сумма: ");
-            Print(A, B);
-        }
-        else if (IsDouble(N) == false) {
-            int A1 = Integer.parseInt(this.RealPart);
-            int A2 = Integer.parseInt(N.RealPart);
-
-            String l1 = "";
-            char[] ds = this.ImaginaryPart.toCharArray();
-            for (int i = 0; i < this.ImaginaryPart.length(); i++) {
-                if (inArray(ds[i],NutNumb) == true) {
-                    l1 += ds[i];
-                }
-            }
-            int B1 = Integer.parseInt(l1);
-
-            String l2 = "";
-            char[] dc = N.ImaginaryPart.toCharArray();
-            for (int j = 0; j < N.ImaginaryPart.length(); j++) {
-                if (inArray(dc[j],NutNumb) == true) {
-                    l2 += dc[j];
-                }
-            }
-            int B2 = Integer.parseInt(l2);
-            int A = A1+A2; int B = B1+B2;
-            System.out.print("Сумма: ");
-            Print(A,B);
-        }
-    }
-    public void Substraction(ComplexNumber N) {
-        if (IsDouble(N) == true) {
-            //---------------------------------
-            double A1 = Double.parseDouble(this.RealPart);
-            double A2 = Double.parseDouble(N.RealPart);
-
-            String l1 = "";
-            char[] ds = this.ImaginaryPart.toCharArray();
-            for (int i = 0; i < this.ImaginaryPart.length(); i++) {
-                if (inArray(ds[i],NutNumb) == true) {
-                    l1 += ds[i];
-                }
-            }
-            int B1 = Integer.parseInt(l1);
-
-            String l2 = "";
-            char[] dc = N.ImaginaryPart.toCharArray();
-            for (int j = 0; j < N.ImaginaryPart.length(); j++) {
-                if (inArray(dc[j],NutNumb) == true) {
-                    l2 += dc[j];
-                }
-            }
-            double B2 = Double.parseDouble(l2);
-            //---------------------------------
-            double B = B1-B2; double A = A1-A2;
-            System.out.print("Разность: ");
-            Print(A,B);
-        }
-        else if (IsDouble(N) == false) {
-            int A1 = Integer.parseInt(this.RealPart);
-            int A2 = Integer.parseInt(N.RealPart);
-
-            String l1 = "";
-            char[] ds = this.ImaginaryPart.toCharArray();
-            for (int i = 0; i < this.ImaginaryPart.length(); i++) {
-                if (inArray(ds[i],NutNumb) == true) {
-                    l1 += ds[i];
-                }
-            }
-            int B1 = Integer.parseInt(l1);
-
-            String l2 = "";
-            char[] dc = N.ImaginaryPart.toCharArray();
-            for (int j = 0; j < N.ImaginaryPart.length(); j++) {
-                if (inArray(dc[j],NutNumb) == true) {
-                    l2 += dc[j];
-                }
-            }
-            int B2 = Integer.parseInt(l2);
-            int A = A1-A2; int B = B1-B2;
-            System.out.print("Разность: ");
-            Print(A,B);
-        }
-    }
-    public void Multiplicate(ComplexNumber N) {
-        if (IsDouble(N) == true) {
-            //---------------------------------
-            double A1 = Double.parseDouble(this.RealPart);
-            double A2 = Double.parseDouble(N.RealPart);
-
-            String l1 = "";
-            char[] ds = this.ImaginaryPart.toCharArray();
-            for (int i = 0; i < this.ImaginaryPart.length(); i++) {
-                if (inArray(ds[i],NutNumb) == true) {
-                    l1 += ds[i];
-                }
-            }
-            int B1 = Integer.parseInt(l1);
-
-            String l2 = "";
-            char[] dc = N.ImaginaryPart.toCharArray();
-            for (int j = 0; j < N.ImaginaryPart.length(); j++) {
-                if (inArray(dc[j],NutNumb) == true) {
-                    l2 += dc[j];
-                }
-            }
-            double B2 = Double.parseDouble(l2);
-            //---------------------------------
-            double B = B1-B2; double A = A1-A2;
-            System.out.print("Произведение: ");
-            Print(A,B);
-        }
-        else if (IsDouble(N) == false) {
-            int A1 = Integer.parseInt(this.RealPart);
-            int A2 = Integer.parseInt(N.RealPart);
-
-            String l1 = "";
-            char[] ds = this.ImaginaryPart.toCharArray();
-            for (int i = 0; i < this.ImaginaryPart.length(); i++) {
-                if (inArray(ds[i],NutNumb) == true) {
-                    l1 += ds[i];
-                }
-            }
-            int B1 = Integer.parseInt(l1);
-
-            String l2 = "";
-            char[] dc = N.ImaginaryPart.toCharArray();
-            for (int j = 0; j < N.ImaginaryPart.length(); j++) {
-                if (inArray(dc[j],NutNumb) == true) {
-                    l2 += dc[j];
-                }
-            }
-            int B2 = Integer.parseInt(l2);
-            int A = A1*A2-B1*B2; int B = A1*B2+B1*A2;
-            System.out.print("Произведение: ");
-            Print(A,B);
+        else if ((a != 0) && (b == 0)) {
+            System.out.println(a);
         }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,15 @@
 package com.company;
 import java.util.Scanner;
-
+import java.util.zip.CheckedOutputStream;
 
 
 public class Main {
 
     public static void main(String[] args) {
-	ComplexNumber One = new ComplexNumber();
-	ComplexNumber Two = new ComplexNumber();
-	One.Entering();
-	Two.Entering();
-	One.Addition(Two);
-	One.Substraction(Two);
-	One.Multiplicate(Two);
+		ComplexNumber one = new ComplexNumber(-1.5,2.5);
+		ComplexNumber two = new ComplexNumber(3.5,-4.5);
+		one.addition(two);
+		one.Substraction(two);
+		one.multiplicate(two);
     }
 }
